5166e98a5c76c461b9b2b133eb2d6053bf43c167
Rick <96212256+RicardoIm1@users.noreply.github.com>
2026-05-18 12:52:55 -0600

    ok

--- js/api.js
+++ js/api.js
@@ -201,8 +201,6 @@ class API {
       const usuarioStr = localStorage.getItem('usuario');
       if (!usuarioStr) return null;
       const usuario = JSON.parse(usuarioStr);
-
-      // Verificar que no sea el string "undefined"
       if (usuario === 'undefined' || !usuario.email) return null;
       return usuario;
     } catch (e) {
